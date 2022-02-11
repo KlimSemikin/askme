47cac407fb3597a37a0fef3b3f60e781c0043fa1
KlimSemikin <klimsemikin@gmail.com>
2022-02-11 09:49:09 +0600

    add update method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,12 @@ class QuestionsController < ApplicationController
       user_id: params[:question][:user_id]
     )
   end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
 end
