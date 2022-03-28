7615bdcefee7f80eabfba50085bd36b6812ea6da
KlimSemikin <klimsemikin@gmail.com>
2022-03-28 15:27:41 +0300

    clean code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # post '/questions', to: 'questions#create'
-  # patch '/questions/:id', to: 'questions#update'
-  # delete 'questions/:id', to: 'questions#destroy'
-
   resources :questions
 end
